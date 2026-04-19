fe79751246be609ee154d1a35a584f39592ee145
Maria Mair <mm225mz@student.lnu.se>
2026-04-19 21:23:51 +0200

    Simplify homepage and styling

--- src/app/page.js
+++ src/app/page.js
@@ -9,15 +9,11 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <div className={styles.intro}>
-          <h1>Shoreline Buildings</h1>
-          <p className={styles.text}>Interactive visualization of data about shoreline buildings in Sweden.</p>
-          <p className={styles.text}>Sign in to go to the dashboard.</p>
-          <SignInButton></SignInButton>
-        </div>
-      </main>
-    </div>
+    <main className={styles.main}>
+      <h1>Shoreline Buildings</h1>
+      <p className={styles.text}>Interactive visualization of data about shoreline buildings in Sweden.</p>
+      <p className={styles.text}>Sign in to go to the dashboard.</p>
+      <SignInButton />
+    </main>
   )
 }
