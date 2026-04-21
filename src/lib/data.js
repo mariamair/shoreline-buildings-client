d29bf2826536948c121c68afe6ea5ecf79e56c25
Maria Mair <mm225mz@student.lnu.se>
2026-04-21 19:34:48 +0200

    Fetch data from GraphQL API

---
+++ src/lib/data.js
@@ -0,0 +1,41 @@
+/**
+ * Fetches the data from the API.
+ *
+ * @author Maria Mair <mm225mz@student.lnu.se>
+ */
+
+import { BUILDING_COUNT_QUERY } from './queries'
+
+export async function getBuildingCountEntities(filter) {
+  const queryUrl = process.env.QUERY_URL
+  const variables = { 
+    regionTypeId: filter.regionTypeId, 
+    areaTypeId: filter.areaTypeId, 
+    buildingTypeId: filter.buildingTypeId, 
+    shorelineTypeId: filter.shorelineTypeId,
+    year: filter.year,
+    limit: filter.limit,
+    offset: filter.offset
+  }
+  
+  try {
+    const response = await fetch(`${queryUrl}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ query: BUILDING_COUNT_QUERY, variables })
+    })
+
+    const { data } = await response.json()
+
+    if (!response.ok) {
+      throw new Error(response)
+    }
+
+    return data
+
+  } catch (error) {
+    console.log(error)
+  }
+}
---
+++ src/lib/queries.js
@@ -0,0 +1,62 @@
+/**
+ * Contains the GraphQL queries.
+ *
+ * @author Maria Mair <mm225mz@student.lnu.se>
+ */
+
+export const BUILDING_COUNT_QUERY = `
+query BuildingCountEntitiesWithFilters (
+  $regionCode: String,
+  $regionTypeId: Int,
+  $year: Int,
+  $areaTypeId: Int,
+  $buildingTypeId: Int,
+  $shorelineTypeId: Int,
+  $limit: Int,
+  $offset: Int) {
+  buildingCountEntities(
+    filter: {
+    regionCode: $regionCode,
+    regionTypeId: $regionTypeId, 
+    year: $year, 
+    areaTypeId: $areaTypeId, 
+    buildingTypeId: $buildingTypeId, 
+    shorelineTypeId: $shorelineTypeId
+    }
+    limit: $limit
+    offset: $offset
+  ) {
+    totalCount
+    limit
+    offset
+    hasNextPage
+    items {
+      id
+      region {
+        code
+        name
+        regionType {
+          id
+          name
+        }
+      }
+      buildingType {
+        id
+        name
+      }
+      areaType {
+        id
+        name
+      }
+      shorelineType {
+        id
+        name
+      }
+      year
+      buildingCount
+      createdAt
+      updatedAt
+    }
+  }
+}
+`
