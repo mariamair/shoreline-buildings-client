4412a1bbad391620d4281a4fd1e3d7c4e43f01fc
Maria Mair <mm225mz@student.lnu.se>
2026-04-26 10:19:11 +0200

    Add region selection to bar chart

--- src/app/dashboard/barChart/page.js
+++ src/app/dashboard/barChart/page.js
@@ -9,6 +9,8 @@
 import { useContext, useEffect, useState } from 'react'
 import { FilterContext } from '../context/FilterContext'
 import { fetchData } from '../actions.js'
+import SelectRegion from '../components/SelectRegion'
+import SelectRegionType from '../components/SelectRegionType'
 import SelectYear from '../components/SelectYear'
 import BarChart from '../components/BarChart'
 import styles from '../page.module.css'
@@ -20,8 +22,24 @@ export default function Numbers() {
   // Get filter values from context
   const { filterValues, setFilterValues } = useContext(FilterContext)
 
+  // Set "inom formellt skyddad natur" (areaTypeId: 3) as area type
   const areaTypeProtected = 3
   filterValues.areaTypeId = areaTypeProtected
+  
+  // Fetch data for the selected region
+  const handleRegionChange = (event) => {
+    const selectedRegionCode = event.target.value
+    setFilterValues({ ...filterValues, parentRegionCode: selectedRegionCode })
+  }
+
+  // Display region selection only when "Kommuner" (regionTypeId: 3) is selected
+  let displayRegionSelection = false
+  if (filterValues.regionTypeId === 3) {
+    displayRegionSelection = true
+  } else {
+    displayRegionSelection = false
+    delete filterValues.parentRegionCode
+  }
 
   useEffect(() => {
     const loadData = async () => {
@@ -38,17 +56,24 @@ export default function Numbers() {
     loadData()
   }, [filterValues])
 
-
   return (
     <main className={styles.main}>
       <h1>Dashboard</h1>
       <h3>Buildings in a protected area</h3>
       <div className={styles.filter}>
+        <SelectRegionType 
+          value={filterValues.regionTypeId} 
+          onChange={(regionTypeId) => setFilterValues({ ...filterValues, regionTypeId })} />
+        {displayRegionSelection && (
+          <SelectRegion 
+            value={filterValues.parentRegionCode || '01'} 
+            onChange={handleRegionChange} />
+        )}
         <SelectYear 
           value={filterValues.year} 
           onChange={(year) => setFilterValues({ ...filterValues, year })} />
       </div>
-      {loading && <p>Loading...</p>}
+      {loading && <p className={styles.loading}>Loading data...</p>}
       {data && <BarChart data={data.buildingCountEntities.items} filterValues={filterValues} />}
     </main>
   )
